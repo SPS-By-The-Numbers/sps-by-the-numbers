43361686edeb2661d33c6af9223939df47d142b8
Albert J. Wong <awong.dev@gmail.com>
2022-04-30 08:34:21 -0700

    Remove next.js boilerplate. Add link to board letter with survey result highlights

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -7,4 +17,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
