98f68c317d10be61f6190387bf1ee6aabb085980
Albert J. Wong <awong.dev@gmail.com>
2022-05-19 22:06:00 -0700

    fix main page link

--- components/Nav.js
+++ components/Nav.js
@@ -59,13 +59,14 @@ export default function Nav() {
       <div class="sm:hidden" id="mobile-menu">
         <div class="px-2 pt-2 pb-3 space-y-1">
           {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-          <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+          <a href="/" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
 
-          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+          <a href="/bell-times" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Bell-Time Survey Results</a>
 
-          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+          <a href="/tools/panorama-slicer" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Panorama Slicer</a>
 
-          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+          <a target="_blank" href="https://andrewbcooper.shinyapps.io/spsstarttimes/" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Andy's Start-Times Tool ⧉</a>
+          <a href="/hcc-northend-resegregation" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">HCC Removal Resegregates Northend</a>
         </div>
       </div>
     </nav>
