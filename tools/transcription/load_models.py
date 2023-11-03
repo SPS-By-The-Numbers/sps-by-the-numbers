6c75707290f2a2af1b1edf6cdb96302275f38cfd
Albert J. Wong <awong.dev@gmail.com>
2023-11-03 00:50:00 -0700

    Delete unneeded files

--- tools/transcription/load_models.py
+++
@@ -1,7 +0,0 @@
-from pyannote.audio import Pipeline
-import whisper
-
-# Cache the models. Useful for creating the Docker image.
-Pipeline.from_pretrained("pyannote/speaker-diarization-3.0", use_auth_token="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")
-whisper.load_model('large-v2')
-
