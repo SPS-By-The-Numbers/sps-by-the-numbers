fcc73566090c0b49686e511c6651c7b4bd9bedda
Mark Verrey <mverrey@gmail.com>
2023-11-07 19:59:55 -0800

    Add types to path-utils

--- utilities/path-utils.ts
+++ utilities/path-utils.ts
@@ -2,22 +2,22 @@ import { format, parse, startOfDay } from 'date-fns';
 
 const pathDateFormat = 'yyyy-MM-dd';
 
-export function formatDateForPath(date) {
+export function formatDateForPath(date: Date): string {
     return format(date, pathDateFormat);
 }
 
-export function parseDateFromPath(dateString) {
+export function parseDateFromPath(dateString: string): Date {
     return startOfDay(parse(dateString, pathDateFormat, new Date()));
 }
 
-export function getCategoryPath(category) {
+export function getCategoryPath(category: string): string {
     return `/transcripts/${category}`;
 }
 
-export function getDatePath(category, date) {
+export function getDatePath(category: string, date: Date): string {
     return `${getCategoryPath(category)}/${formatDateForPath(date)}`;
 }
 
-export function getTranscriptPath(category, date, videoId) {
+export function getTranscriptPath(category: string, date: Date, videoId: string): string {
     return `${getDatePath(category, date)}/${videoId}`;
 }
